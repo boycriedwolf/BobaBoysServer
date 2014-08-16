327e5be3b76a087076fbf4048ddab944158cb4bd
Byron Tang <byronyugontang@gmail.com>
2014-08-15 17:13:08 -0700

    edited retrieve address to return map of the five addresses

--- src/main/java/HelloBoba/Server/RetrieveAddressServlet.java
+++ src/main/java/HelloBoba/Server/RetrieveAddressServlet.java
@@ -10,6 +10,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -28,16 +30,13 @@ public class RetrieveAddressServlet extends HttpServlet{
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
 		String jsonReqString = "";
 		int userId = 0;
-		String defaultString = "default";
-		String defaultDeliveryAddress = "default";
-		String affiliation = "default";
 
 		try {
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(request.getInputStream()));
 			if(inFromClient != null) {
 				jsonReqString = inFromClient.readLine();
 			}
-		}catch (IOException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -46,25 +45,10 @@ public class RetrieveAddressServlet extends HttpServlet{
 		userId = jsonObj.getInt("user_id");
 
 		JSONObject jsonResObj = new JSONObject();
-
-		defaultDeliveryAddress = retrieveDefaultAddress(userId);
-//		affiliation = retrieveAffiliation(userId);
-//		if(!defaultString.equalsIgnoreCase(affiliation)) {
-			if(!defaultString.equalsIgnoreCase(defaultDeliveryAddress)) {
-//				if(affiliation != null) {
-//					jsonResObj.put("affiliation", affiliation);
-//				}
-//				else jsonResObj.put("affiliation", ServerConstants.NO_AFFILIATION);
-				if(defaultDeliveryAddress != null) {
-					jsonResObj.put("default_delivery_address", defaultDeliveryAddress);
-				}
-				else jsonResObj.put("default_delivery_address", ServerConstants.NO_DEFAULT_DELIVERY_ADDRESS);
-//				jsonResObj.put(ServerConstants.REQUEST_STATUS, ServerConstants.AFFILIATION_AND_ADDRESS_RETRIEVE_SUCCESS);
-			}
-			else jsonResObj.put(ServerConstants.REQUEST_STATUS, ServerConstants.RETRIEVE_ADDRESS_FAIL);
-//		}
-//		else jsonResObj.put(ServerConstants.REQUEST_STATUS, ServerConstants.RETRIEVE_AFFILIATION_FAIL);
-
+		Map<String, String> addressMap = retrieveUserAddress(userId);
+		
+		jsonResObj.put("default_delivery_address", addressMap);	
+		
 		response.setContentType("application/json");
 		String jsonResString = jsonResObj.toString();
 
@@ -81,38 +65,53 @@ public class RetrieveAddressServlet extends HttpServlet{
 
 	}
 
-//	private String retrieveAffiliation(int userId) {
-//		Connection con = MiscMethods.establishDatabaseConnection();
-//		PreparedStatement ps;
-//		String affiliation = null;
-//		try {
-//			ps = con.prepareStatement("SELECT affiliation FROM " + ServerConstants.DB_USER_TABLE + " WHERE user_id = ?");
-//			ps.setInt(1, userId);
-//			ResultSet rs = ps.executeQuery();
-//			if(rs.next()) {
-//				affiliation = rs.getString(1);
-//			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		return affiliation;
-//	}
-
-	private String retrieveDefaultAddress(int userId) {
+	private Map<String, String> retrieveUserAddress(int userId) {
 		Connection con = MiscMethods.establishDatabaseConnection();
 		PreparedStatement ps;
-		String defaultDeliveryAddress = null;
+		Map<String, String> map = new HashMap<String, String>();
+		String defaultAddress = "";
+		String address2 = "";
+		String address3 = "";
+		String address4 = "";
+		String address5 = "";
+		
 		try {
-			ps = con.prepareStatement("SELECT default_delivery_address FROM " + ServerConstants.DB_USER_TABLE + " WHERE user_id = ?");
+			ps = con.prepareStatement("SELECT * FROM " + ServerConstants.DB_USER_ADDRESS_TABLE +
+					" WHERE user_id = ?");
 			ps.setInt(1, userId);
 			ResultSet rs = ps.executeQuery();
 			if(rs.next()) {
-				defaultDeliveryAddress = rs.getString(1);
+				defaultAddress = rs.getString("default_address"); 
+				address2 = rs.getString("address_2");
+				address3 = rs.getString("address_3");
+				address4 = rs.getString("address_4");
+				address5 = rs.getString("address_5");
+				if(defaultAddress == null) {
+					map.put("default_address", "none");
+				}
+				else map.put("default_address", defaultAddress);
+				if(address2 == null) {
+					map.put("address_2", "none");
+				}
+				else map.put("address_2", address2);
+				if(address3 == null) {
+					map.put("address_3", "none");
+				}
+				else map.put("address_3", address3);
+				if(address4 == null) {
+					map.put("address_4", "none");
+				}
+				else map.put("address_4", address4);
+				if(address5 == null) {
+					map.put("address_5", "none");
+				}
+				else map.put("address_5", address5);
 			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return defaultDeliveryAddress;
+		return map;
 	}
 
 
